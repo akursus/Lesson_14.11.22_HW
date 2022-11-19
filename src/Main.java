7853f4b9dca7437604d1c6552525d85932cf8cf8
akursus <ak_ursus@list.ru>
2022-11-19 18:49:40 +0300

    Task 2

--- src/Main.java
+++ src/Main.java
@@ -3,11 +3,28 @@ public class Main {
         //task1
         System.out.println("Задание 1");
         int age = 19;
+        System.out.println("Возраст = " +age +" лет");
         if (age>=18) {
             System.out.println("Поздравляем Вас с совершеннолетием!");
         }
         if (age<18) {
             System.out.println("Возраст совершеннолетия ещё не наступил, и Вам нужно немного подождать");
+        }
+        //task2
+        System.out.println("Задание 2");
+        int personAge = 23;
+        System.out.println("Возраст = " +personAge +" лет");
+        if (personAge<7) {
+            System.out.println("Ребёнок ещё мал для школы");
+        }
+        if (personAge>=7 && personAge<18) {
+            System.out.println("Ребёнок ходит в школу");
+        }
+        if (personAge>=18 && personAge<24) {
+            System.out.println("Человек уже закончил школу и может отправляться в университет");
+        }
+        if (personAge>=24) {
+            System.out.println("Человек окончил университет и ему пора искать первую работу");
         }
     }
 }
