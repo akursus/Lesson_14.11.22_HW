4af5b307b6e12b3898a213df9b303dfaa1376f7d
akursus <ak_ursus@list.ru>
2022-11-19 18:22:48 +0300

    Task 1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        //task1
+        System.out.println("Задание 1");
+        int age = 19;
+        if (age>=18) {
+            System.out.println("Поздравляем Вас с совершеннолетием!");
+        }
+        if (age<18) {
+            System.out.println("Возраст совершеннолетия ещё не наступил, и Вам нужно немного подождать");
+        }
     }
 }
